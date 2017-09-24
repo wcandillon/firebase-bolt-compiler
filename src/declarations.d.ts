80a7faf8bfeb193744a8441b1f383ecf6bb808a5
William Candillon <wcandillon@gmail.com>
2017-09-24 19:13:02 +0200

    feat(typescript): Generate path functions

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,3 +1,48 @@
 declare module "firebase-bolt" {
-    export function parse(source: string): { schema: {} };
+    export interface PathPart {
+        label: string;
+        variable: string;
+    }
+
+    export type PathTemplate = { parts: PathPart[] };
+
+    export interface Path {
+        template: PathTemplate;
+        isType: ExpType;
+    }
+
+    export type Schemas = { [name: string]: Schema };
+
+    export interface Exp {
+        type: string;
+        valueType: string;
+    }
+
+    export type ExpType = ExpSimpleType | ExpUnionType | ExpGenericType;
+
+    export interface ExpGenericType extends Exp {
+        name: string;
+        params: ExpType[];
+    }
+
+    export interface ExpUnionType extends Exp {
+        types: ExpType[];
+    }
+
+    export interface ExpSimpleType extends Exp {
+        name: string;
+    }
+
+    export interface TypeParams {
+        [name: string]: ExpType;
+    }
+
+    export interface Schema {
+        derivedFrom: ExpType;
+        properties: TypeParams;
+        // Generic parameters - if a Generic schema
+        params?: string[];
+    }
+
+    export function parse(source: string): { schema: Schemas, paths: Path[] };
 }
